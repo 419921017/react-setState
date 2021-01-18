2d6a43ed295ec340aa3a84a99564dd8c248bc123
power_840 <liuyu19921017@gmail.com>
2021-01-18 14:38:05 +0800

    feat: component类

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-class Counter {
-  constructor() {
-    this.state = {
-      number: 0
-    }
+class Component {
+  constructor(props) {
+    this.props = props
+
   }
   createDOMFromDOMString(domString) {
-    const div = document.createElement('div')
-    div.innerHTML = domString
-    return div.children[0]
+    const div = document.createElement('div');
+    div.innerHTML = domString;
+    return div.children[0];
   }
-  add() {
-    this.state = {
-      number: this.state.number + 1
-    }
-    const oldElement = this.domElement
-    const newElement = this.render()
-    this.domElement = newElement
-    oldElement.parentElement.replaceChild(newElement, oldElement)
+
+  setState(intrinsicState) {
+    this.state = Object.assign(this.state, intrinsicState);
+    let oldElement = this.domElement;
+    let newElement = this.render();
+    oldElement.parentElement.replaceChild(newElement, oldElement);
   }
-  render() {
-    this.domElement = this.createDOMFromDOMString(`
-      <button >${this.state.number}</button>
-    `)
-    this.domElement.addEventListener('click', this.add.bind(this))
-    return this.domElement
+
+  renderElement() {
+    let renderString = this.render();
+    this.domElement = this.createDOMFromDOMString(renderString);
+    return this.domElement;
+  }
+
+  mount(container) {
+    container.appendChild(this.renderElement());
   }
 }
 
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      number: 0,
+    };
+  }
+  add() {
+    this.setState({ number: this.state.number + 1 });
+    // this.state = {
+    //   number: this.state.number + 1,
+    // };
+    // const oldElement = this.domElement;
+    // const newElement = this.render();
+    // this.domElement = newElement;
+    // oldElement.parentElement.replaceChild(newElement, oldElement);
+  }
+  render() {
+    console.log(this.mount);
+    return `
+    <button >${this.state.number}</button>
+  `;
+  }
+}
